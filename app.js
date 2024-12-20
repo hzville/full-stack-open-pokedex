8d0f73874b0c914904ea88a9fe952b6ecdf40e73
Ville Halonen <71012693+hzville@users.noreply.github.com>
2024-12-20 22:51:09 +0200

    add /health endpoint

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('App running version: 2')
+  res.send('App running version: 3')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
