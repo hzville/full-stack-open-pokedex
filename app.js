fd56b95187d72f7bb7030589f9d630f8a9da9dff
Ville Halonen <71012693+hzville@users.noreply.github.com>
2024-12-20 23:05:47 +0200

    11.12

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('App running version: 3')
+  res.send('App running version: 4')
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  '
   res.send('ok')
 })
 
