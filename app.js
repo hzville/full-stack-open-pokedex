efdfdc124bc37079745c11f1e2882f70e2ecaea7
Ville Halonen <71012693+hzville@users.noreply.github.com>
2024-12-20 22:25:28 +0200

    11.11

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('App running version: 1')
+  res.send('App running version: 2')
 })
 
 app.listen(PORT, () => {
